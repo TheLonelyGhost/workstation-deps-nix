75ec1e150dc6cf69474d1e542177d5729beb1ef9
David Alexander <opensource@thelonelyghost.com>
2022-02-20 00:00:49 -0500

    Fixes ssh-agent relay

--- packages/wsl/ssh-agent-relay.nix
+++ packages/wsl/ssh-agent-relay.nix
@@ -22,27 +22,15 @@ pkgs.writeShellApplication {
   fi
 
   sock="$rundir/ssh-agent.sock"
-  SSH_AUTH_SOCK="''${SSH_AUTH_SOCK:-$sock}"
 
-  if [ "$SSH_AUTH_SOCK" != "$sock" ] && ! [ -e "$SSH_AUTH_SOCK" ]; then
-    # If it doesn't exist on the filesystem, let's just set it to
-    # our own location instead
-    SSH_AUTH_SOCK="$sock"
+  if [ -e "$sock" ] && ! socat -u OPEN:/dev/null UNIX-CONNECT:"$sock" 1>/dev/null 2>&1; then
+    rm "$sock"
   fi
 
-  if [ -e "$SSH_AUTH_SOCK" ] && ! socat -u OPEN:/dev/null UNIX-CONNECT:"''${SSH_AUTH_SOCK}" 1>/dev/null 2>&1; then
-    # The socket is closed. Let's remove it so we can recreate it
-    # further down
-    rm "''${SSH_AUTH_SOCK}"
+  if ! [ -e "$sock" ]; then
+    ( setsid socat UNIX-LISTEN:"$sock",fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) &>/dev/null
   fi
 
-  if ! [ -e "''${SSH_AUTH_SOCK}" ]; then
-    ( setsid socat UNIX-LISTEN:"''${SSH_AUTH_SOCK}",fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) &>/dev/null
-  fi
-
-  if [ "$SSH_AUTH_SOCK" != "$sock" ]; then
-    # This way we can eval the output safely (as much as `eval` is safe...)
-    printf 'export SSH_AUTH_SOCK=%q\n' "$sock"
-  fi
+  printf 'export SSH_AUTH_SOCK=%q\n' "$sock"
   '';
 }
