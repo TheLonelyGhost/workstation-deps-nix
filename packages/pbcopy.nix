362e063e9724f114ed5a25863e560cdbd4c8d2f6
David Alexander <opensource@thelonelyghost.com>
2022-04-03 22:36:03 -0400

    Adds pbcopy and pbpaste shims for Linux and WSL

---
+++ packages/pbcopy.nix
@@ -0,0 +1,16 @@
+{ pkgs }:
+# vim: set ts=2 sts=2 sw=2 et
+
+pkgs.writeShellApplication {
+  name = "pbcopy";
+  runtimeInputs = (pkgs.lib.optionals (pkgs.stdenv.isLinux) [
+    # see: https://codepre.com/en/como-usar-los-comandos-pbcopy-y-pbpaste-en-linux.html
+    pkgs.xclip
+  ]);
+
+  text = (if pkgs.stdenv.isLinux then ''
+    xclip -selection clipboard
+  '' else ''
+    /usr/bin/pbcopy
+  '');
+}
